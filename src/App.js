0befbf9b730bcb5bd19bfd081b94d07715da622f
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-04-30 20:00:39 +0200

    feat: add globalStateService and example app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { GlobalStateProvider, useGlobalDispatcher, useGlobalState } from './globalStateService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const defineQuestionAC = question => ({
+  type: 'DEFINE_QUESTION',
+  question
+})
+const setAnswerAC = answer => ({
+  type: 'SET_ANSWER',
+  answer
+})
+const defaultState = {
+  answer: 42,
+  question: undefined
 }
+
+const sampleReducer = (state = defaultState, action) => {
+  switch (action?.type) {
+    case 'DEFINE_QUESTION':
+      return {
+        ...state,
+        question: action.question
+      }
+    case 'SET_ANSWER':
+      return {
+        ...state,
+        answer: action.answer
+      }
+    default:
+      return state
+  }
+}
+
+let questionRenderCount = 0
+const questionSelector = state => state.question
+const Question = () => {
+  const [question] = useGlobalState(questionSelector)
+  return <strong>{question} ({++questionRenderCount})</strong>
+}
+
+let answerRenderCount = 0
+const Answer = () => {
+  const [answer] = useGlobalState(state => state.answer) // inline selectors work too
+  return <strong>{answer} ({++answerRenderCount})</strong>
+}
+
+const SolveButton = () => {
+  const defineQuestion = useGlobalDispatcher(defineQuestionAC)
+
+  return <button onClick={() => defineQuestion('What is the meaning of life, the universe, and everything?')}>What is the question?</button>
+}
+const RandomizeButton = () => {
+  const setAnswer = useGlobalDispatcher(setAnswerAC)
+
+  return <button onClick={() => setAnswer(Math.floor(Math.random() * 10000))}>Randomize answer</button>
+}
+
+const App = () => (
+  <GlobalStateProvider rootReducer={sampleReducer}>
+      <div className="outer">
+        <div className="container">
+          Question: <Question />
+          Answer: <Answer />
+          <SolveButton />
+          <RandomizeButton />
+        </div>
+      </div>
+  </GlobalStateProvider>
+)
 
 export default App;
---
+++ src/globalStateService.js
@@ -0,0 +1,65 @@
+import React, { useMemo, useEffect, useCallback, useContext, useState } from 'react'
+
+const GlobalStateContext = React.createContext({
+    state: undefined,
+    dispatch: () => undefined
+})
+
+export const GlobalStateProvider = ({ rootReducer, children }) => {
+    const subscriptions = useMemo(() => new Set(), [])
+
+    const subscribe = useCallback(callback => {
+        subscriptions.add(callback)
+    }, [subscriptions])
+    const unsubscribe = useCallback(callback => {
+        subscriptions.delete(callback)
+    }, [subscriptions])
+    
+    const dispatch = useCallback(action => {
+        stateContext.state = rootReducer(stateContext.state, action)
+        for (let callback of subscriptions) {
+            callback(stateContext.state)
+        }
+    }, [rootReducer, subscriptions, /* ignore stateContext.state updates */]) /* eslint-disable-line react-hooks/exhaustive-deps */
+    
+    const stateContext = useMemo(() => ({
+        state: rootReducer(undefined, undefined),
+        dispatch,
+        subscribe,
+        unsubscribe
+    }), [rootReducer, dispatch, subscribe, unsubscribe])
+
+    return <GlobalStateContext.Provider value={stateContext}>
+        {children}
+    </GlobalStateContext.Provider>
+}
+
+const identitySelector = state => state // By default, respond to ANY state change
+export const useGlobalState = (selector = identitySelector) => {
+    const context = useContext(GlobalStateContext)
+    const [selectedState, setSelectedState] = useState(selector(context.state))
+
+    const stableSelector = useCallback(selector, [ /* ignore selector updates */ ]) /* eslint-disable-line react-hooks/exhaustive-deps */
+
+    useEffect(() => {
+        const callback = state => {
+            setSelectedState(
+                stableSelector(state)
+            )
+        }
+        context.subscribe(callback)
+        return () => context.unsubscribe(callback)
+    }, [context, stableSelector, selectedState])
+
+    return [selectedState, { dispatch: context.dispatch }]
+}
+
+export const useGlobalDispatch = () => {
+    return useContext(GlobalStateContext).dispatch
+}
+export const useGlobalDispatcher = (actionCreator) => {
+    const dispatch = useGlobalDispatch()
+    return useCallback((...args) => {
+        dispatch(actionCreator(...args))
+    }, [actionCreator, dispatch])
+}
